aff2c15b9855ff810f75e64ebe48872d35361e39
gyulinee <rbfls102@gmail.com>
2023-05-10 02:40:44 +0900

    1_Lab02

---
+++ 1_Lab02_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  #11382
+int main() {
+    long int x;
+    long int y;
+    long int z;
+
+    scanf("%ld %ld %ld", &x, &y, &z);
+
+    printf("%ld", x + y + z);
+
+    return 0;
+
+}
---
+++ 1_Lab02_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h> #10869
+int main() {
+    int x;
+    int y;
+
+    scanf("%d %d", &x, &y);
+
+    printf("%d \n", x + y);
+    printf("%d \n", x - y);
+    printf("%d \n", x * y);
+    printf("%d \n", x / y);
+    printf("%d", x % y);
+
+    return 0;
+
+
+}
